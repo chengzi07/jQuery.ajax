1108e820a5fafcebb709ad86dfec0cac45f20525
chengzi07 <yihanchen7@163.com>
2018-10-07 12:29:43 +0800

    ajax的promise形式

--- main.js
+++ main.js
@@ -6,40 +6,26 @@ window.jQuery=function(nodeOrSelector){
 }
 window.$=window.jQuery
 
-window.jQuery.ajax=function(options){
-  let url
-  if(arguments.length===1){
-    url=options.url
-  }else if(arguments.length===2){
-    url=arguments[0]
-    options=arguments[1]
-  }
-  let method=options.method
-  let body=options.body
-  let successFn=options.successFn
-  let failFn=options.failFn
-  let headers=options.headers
-
-  let request=new XMLHttpRequest()
-  request.open(method,url) // 配置request
-  for(let key in headers){
-    let value=headers[key]
-    request.setRequestHeader(key,value)
-  }
-  request.onreadystatechange=()=>{
-    if(request.readyState===4){
-      if(request.status>=200&&request.status<300){
-        successFn.call(undefined,request.responseText)
-      }else if(request.status>=400){
-        failFn.call(undefined,request)
+window.jQuery.ajax=function({url,method,body,headers}){
+  return new Promise(function(resolve,reject){
+    let request=new XMLHttpRequest()
+    request.open(method,url) // 配置request
+    for(let key in headers){
+      let value=headers[key]
+      request.setRequestHeader(key,value)
+    }
+    request.onreadystatechange=()=>{
+      if(request.readyState===4){
+        if(request.status>=200&&request.status<300){
+          resolve.call(undefined,request.responseText)
+        }else if(request.status>=400){
+          reject.call(undefined,request)
+        }
       }
     }
-  }
   request.send(body)
+  })
 }
-
-function f1(responseText){}
-function f2(responseText){}
 
 myButton.addEventListener('click',(e)=>{
   window.jQuery.ajax({
@@ -48,15 +34,9 @@ myButton.addEventListener('click',(e)=>{
     headers:{
       'content-type':'application/x-www-form-urlencoded',
       'frank':'18'
-    },
-    successFn:(x)=>{
-      f1.call(undefined,x)
-      f2.call(undefined,x)
-    },
-    failFn:(x)=>{
-      console.log(x)
-      console.log(x.status)
-      console.log(x.responseText)
     }
-  })
+  }).then(
+    (text)=>{console.log(text)},
+    (request)=>{console.log(request)}
+  )
 })
